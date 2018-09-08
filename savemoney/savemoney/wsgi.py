9bf92f89d84447f44f3232ce90f0f43f8ddef2ed
ivanjo39191 <ivanjo3919@gmail.com>
2018-09-08 15:58:13 +0800

    initial commit

---
+++ savemoney/savemoney/wsgi.py
@@ -0,0 +1,15 @@
+"""
+WSGI config for savemoney project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.1/howto/deployment/wsgi/
+"""
+
+import os
+from celery import Celery
+from django.core.wsgi import get_wsgi_application
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'savemoney.settings')
+
+application = get_wsgi_application()
